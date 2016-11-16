00687946bdd75dbba6a3f753c92f193edacb30fb
Harold Angenent <harold@trendwerk.nl>
2016-11-16 17:39:07 +0100

    Introduce Entry class

---
+++ src/Entry.php
@@ -0,0 +1,25 @@
+<?php
+namespace Trendwerk\AcfForms;
+
+final class Entry
+{
+    private $id;
+    private $keys = [
+        'fieldGroups' => '_fieldGroups',
+    ];
+
+    private function __construct($id)
+    {
+        $this->id = $id;
+    }
+
+    public static function find($id)
+    {
+        return new static($id);
+    }
+
+    public function setFieldGroups($fieldGroups)
+    {
+        update_post_meta($this->id, $this->keys['fieldGroups'], $fieldGroups);
+    }
+}
