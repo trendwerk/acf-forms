f5168fd67f426ebb4dee000b5100738c22276d00
Harold Angenent <harold@trendwerk.nl>
2016-11-22 13:49:37 +0100

    Test find method for Entry

---
+++ tests/AcfForms/EntryTest.php
@@ -0,0 +1,25 @@
+<?php
+namespace Trendwerk\AcfForms\Test;
+
+use Trendwerk\AcfForms\Entries;
+use Trendwerk\AcfForms\Entry;
+
+class EntryTest extends \WP_UnitTestCase
+{
+    private $postId;
+
+    public function setUp()
+    {
+        parent::setUp();
+
+        $this->postId = $this->factory->post->create([
+            'post_type' => Entries::POST_TYPE,
+        ]);
+    }
+
+    public function testFind()
+    {
+        $entry = Entry::find($this->postId);
+        $this->assertEquals('Trendwerk\AcfForms\Entry', get_class($entry));
+    }
+}
