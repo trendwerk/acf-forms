e722f5c79403789863c5db8c950726dd9455a0e8
Harold Angenent <harold@trendwerk.nl>
2016-11-22 15:39:16 +0100

    Move deprecated Handler code to Database handler

--- src/Handlers/Database.php
+++ src/Handlers/Database.php
@@ -5,5 +5,14 @@ final class Database implements HandlerInterface
 {
     public function handle($entry)
     {
+        if (! isset($_POST['fieldGroups'])) {
+            return;
+        }
+
+        $fieldGroups = array_map('esc_attr', json_decode(stripslashes($_POST['fieldGroups'])));
+
+        if ($fieldGroups) {
+            $entry->setFieldGroups($fieldGroups);
+        }
     }
 }
