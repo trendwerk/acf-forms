f5bce83c3cd7efbdd495684843d588d51325853f
Harold Angenent <harold@trendwerk.nl>
2016-11-23 13:31:46 +0100

    Only render tab divider for second to last tabs

--- src/Renderers/Tab.php
+++ src/Renderers/Tab.php
@@ -3,8 +3,30 @@ namespace Trendwerk\AcfForms\Renderers;
 
 class Tab extends Field implements RendererInterface
 {
+    protected static $first = true;
+
     public function render($entry)
     {
-        return "<hr><h2>{$this->field['label']}</h2>";
+        $output = '';
+
+        if ($this->shouldRenderDivider()) {
+            $output .= $this->divider();
+        }
+
+        self::$first = false;
+
+        $output .= "<h2>{$this->field['label']}</h2>";
+
+        return $output;
+    }
+
+    protected function divider()
+    {
+        return '<hr>';
+    }
+
+    private function shouldRenderDivider()
+    {
+        return ! self::$first;
     }
 }
