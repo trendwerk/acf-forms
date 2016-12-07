d52d0ed9ee1ecca55e28f0750a026428077d36af
Harold Angenent <harold@trendwerk.nl>
2016-12-07 17:46:48 +0100

    ✅ Add Forms tests for add(), get() and exceptions

--- tests/AcfForms/Form/FormsTest.php
+++ tests/AcfForms/Form/FormsTest.php
@@ -2,6 +2,7 @@
 namespace Trendwerk\AcfForms\Test;
 
 use BadMethodCallException;
+use InvalidArgumentException;
 use Trendwerk\AcfForms\Form\Forms;
 
 class FormsTest extends \WP_UnitTestCase
@@ -17,5 +18,27 @@ class FormsTest extends \WP_UnitTestCase
     {
         $this->expectException(BadMethodCallException::class);
         $this->forms->add('test', []);
+    }
+
+    public function testAdd()
+    {
+        $name = 'test';
+        $fieldGroups = ['test_field_group'];
+
+        $this->forms->add($name, [
+            'acfForm'          => [
+                'field_groups' => $fieldGroups,
+            ],
+        ]);
+
+        $form = $this->forms->get($name);
+
+        $this->assertEquals($form['acfForm']['field_groups'], $fieldGroups);
+    }
+
+    public function testInvalidGet()
+    {
+        $this->expectException(InvalidArgumentException::class);
+        $this->forms->get('testGet');
     }
 }
