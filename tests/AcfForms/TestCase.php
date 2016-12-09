cc51616565d6bffde5b5d3ec5fd5d646438c5b03
Harold Angenent <harold@trendwerk.nl>
2016-12-09 13:53:44 +0100

    ✅ Update TestCase to ACF Pro

--- tests/AcfForms/TestCase.php
+++ tests/AcfForms/TestCase.php
@@ -9,13 +9,13 @@ abstract class TestCase extends \WP_UnitTestCase
             $fields = [
                 [
                     'key'  => 'field_' . uniqid(),
-                    'name' => 'Text',
+                    'name' => 'text',
                     'type' => 'text',
                 ],
             ];
         }
 
-        register_field_group([
+        acf_add_local_field_group([
             'key'    => $name,
             'fields' => $fields,
         ]);
@@ -23,14 +23,6 @@ abstract class TestCase extends \WP_UnitTestCase
 
     protected function getFieldGroup($name)
     {
-        if (! empty($GLOBALS['acf_register_field_group'])) {
-            return array_reduce($GLOBALS['acf_register_field_group'], function ($carry, $item) use ($name) {
-                if ($item['key'] == $name) {
-                    return $item;
-                }
-
-                return $carry;
-            });
-        }
+        return acf_get_local_field_group($name);
     }
 }
