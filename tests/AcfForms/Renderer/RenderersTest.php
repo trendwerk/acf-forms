1cbda91689d7f34e3dac10fd23adab67713ff364
Harold Angenent <harold@trendwerk.nl>
2016-12-16 16:59:04 +0100

    ✅ Test post object

--- tests/AcfForms/Renderer/RenderersTest.php
+++ tests/AcfForms/Renderer/RenderersTest.php
@@ -30,4 +30,42 @@ class RenderersTest extends TestCase
         $this->assertContains($field['value'], $renderer->render($this->entry));
         $this->assertNotContains($field['label'], $renderer->render($this->entry, false));
     }
+
+    public function testPostObject()
+    {
+        $post = $this->factory->post->create_and_get();
+
+        $field = [
+            'key'           => 'field_post',
+            'label'         => 'Post Object',
+            'type'          => 'post_object',
+            'value'         => $post,
+            'return_format' => 'object',
+        ];
+
+        $renderer = FieldFactory::create($field);
+
+        $this->assertContains($field['label'], $renderer->render($this->entry));
+        $this->assertContains($post->post_title, $renderer->render($this->entry));
+        $this->assertNotContains($field['label'], $renderer->render($this->entry, false));
+    }
+
+    public function testPostObjectReturnId()
+    {
+        $post = $this->factory->post->create_and_get();
+
+        $field = [
+            'key'           => 'field_post',
+            'label'         => 'Post Object',
+            'type'          => 'post_object',
+            'value'         => $post->ID,
+            'return_format' => 'id',
+        ];
+
+        $renderer = FieldFactory::create($field);
+
+        $this->assertContains($field['label'], $renderer->render($this->entry));
+        $this->assertContains($post->post_title, $renderer->render($this->entry));
+        $this->assertNotContains($field['label'], $renderer->render($this->entry, false));
+    }
 }
