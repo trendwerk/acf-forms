1e995823746c0c7c5cbed9573ffe7f56b36af11f
Harold Angenent <harold@trendwerk.nl>
2021-01-22 15:23:41 +0100

    👽 Update changed filters from ACF

--- src/Entry/Rule.php
+++ src/Entry/Rule.php
@@ -12,8 +12,7 @@ final class Rule
     {
         add_filter('acf/location/rule_types', [$this, 'add']);
         add_filter('acf/location/rule_values/' . $this->rule, [$this, 'values']);
-        add_filter('acf/location/screen', [$this, 'addFieldGroup'], 10, 2);
-        add_filter('acf/location/rule_match/' . $this->rule, [$this, 'match'], 10, 3);
+        add_filter('acf/location/rule_match/' . $this->rule, [$this, 'match'], 10, 4);
     }
 
     public function add(array $rules)
@@ -30,17 +29,10 @@ final class Rule
         return $values;
     }
 
-    public function addFieldGroup(array $options, array $fieldGroup)
-    {
-        $options['fieldGroup'] = $fieldGroup;
-
-        return $options;
-    }
-
-    public function match($match, array $rule, array $args)
+    public function match($match, array $rule, array $args, array $fieldGroup)
     {
         $entry = Entry::find($args['post_id']);
-        $match = in_array($args['fieldGroup']['key'], $entry->getFieldGroups());
+        $match = in_array($fieldGroup['key'], $entry->getFieldGroups());
 
         if ($rule['operator'] === '!=') {
             return ! $match;
